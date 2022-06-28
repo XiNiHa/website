23d263c4917807ea1fbc3ab9ac222c0db533bc83
XiNiHa <me@xiniha.dev>
2022-06-28 22:43:54 +0900

    아이콘 로딩 방식 개선

--- src/components/SmoothIcon.tsx
+++ src/components/SmoothIcon.tsx
@@ -9,20 +9,17 @@ type Props = {
 
 const SmoothIcon: React.FC<Props> = ({ iconUrl, iconAlt, className }) => {
   const [visible, setVisible] = React.useState(false)
-  const imageRef = React.useRef<HTMLImageElement>(null)
 
   React.useEffect(() => {
-    const listener = () => setVisible(true)
-    const currentImageRef = imageRef.current
-    currentImageRef?.addEventListener('load', listener)
-    return () => currentImageRef?.removeEventListener('load', listener)
-  }, [setVisible])
+    const image = new Image()
+    image.addEventListener('load', () => setVisible(true))
+    image.src ||= iconUrl
+  }, [])
 
   return (
     <span className={className}>
       <img
-        ref={imageRef}
-        src={iconUrl}
+        src={visible ? iconUrl : undefined}
         alt={iconAlt}
         className="h-full transition-opacity duration-500"
         style={{ opacity: visible ? 1 : 0 }}
