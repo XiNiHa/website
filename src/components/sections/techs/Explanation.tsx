7b4b4fc729acc35abb7d86aa2b34731c23763d28
XiNiHa <me@xiniha.dev>
2022-08-04 19:15:38 +0900

    Techs 섹션 크기 이슈 수정

--- src/components/sections/techs/Explanation.tsx
+++ src/components/sections/techs/Explanation.tsx
@@ -1,4 +1,4 @@
-import { batch, Component, createEffect, createSignal, untrack } from 'solid-js'
+import { batch, Component, createEffect, createReaction, createSignal, untrack } from 'solid-js'
 import { activeTech } from '@/state/tech'
 
 interface Props {
@@ -8,6 +8,8 @@ interface Props {
 const Explanation: Component<Props> = ({ explanations }) => {
   const [textA, setTextA] = createSignal('')
   const [textB, setTextB] = createSignal('')
+  const [textAHeight, setTextAHeight] = createSignal(0)
+  const [textBHeight, setTextBHeight] = createSignal(0)
   const [activeText, setActiveText] = createSignal<'a' | 'b'>('a')
 
   createEffect(() => {
@@ -33,24 +35,38 @@ const Explanation: Component<Props> = ({ explanations }) => {
   const textClasses =
     'text-xl leading-[1.8] absolute transition-all duration-500'
 
+  const textAEl = (
+    <div
+      innerHTML={textA()}
+      class={textClasses}
+      classList={{
+        'opacity-100': activeText() === 'a',
+        'opacity-0': activeText() !== 'a',
+      }}
+    />
+  ) as HTMLElement
+  const textBEl = (
+    <div
+      innerHTML={textB()}
+      class={textClasses}
+      classList={{
+        'opacity-100': activeText() === 'b',
+        'opacity-0': activeText() !== 'b',
+      }}
+    />
+  ) as HTMLElement
+
+  createReaction(() => {
+    batch(() => {
+      setTextAHeight(textAEl.clientHeight)
+      setTextBHeight(textBEl.clientHeight)
+    })
+  })(() => (textA(), textB()))
+
   return (
-    <div>
-      <div
-        innerHTML={textA()}
-        class={textClasses}
-        classList={{
-          'opacity-100': activeText() === 'a',
-          'opacity-0': activeText() !== 'a',
-        }}
-      />
-      <div
-        innerHTML={textB()}
-        class={textClasses}
-        classList={{
-          'opacity-100': activeText() === 'b',
-          'opacity-0': activeText() !== 'b',
-        }}
-      />
+    <div style={{ height: Math.max(textAHeight(), textBHeight()) + 'px' }} class="transition-all duration-500">
+      {textAEl}
+      {textBEl}
     </div>
   )
 }
