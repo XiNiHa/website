f3edf58224f27a9b03d73bb13c96bd19c66e472f
XiNiHa <me@xiniha.dev>
2022-08-05 21:38:20 +0900

    반응형 대응

--- src/components/sections/experiences/Item.tsx
+++ src/components/sections/experiences/Item.tsx
@@ -63,13 +63,13 @@ const Item: Component<Props> = ({ experience, subprojectMap }) => {
       class={'flex flex-col ' + (experience.stack ? 'gap-1' : 'gap-1 md:gap-2')}
     >
       <div
-        class="flex items-stretch gap-2 cursor-pointer w-fit mb-1 transition-colors duration-300 border-b border-b-transparent hover:border-b-gray-400 sm:flex-nowrap"
-        classList={{ 'flex-wrap': experience.isSubproject }}
+        class="flex items-center gap-2 cursor-pointer w-fit mb-1 transition-colors duration-300 border-b border-b-transparent hover:border-b-gray-400 sm:flex-nowrap"
+        classList={{ 'flex-wrap': !experience.isSubproject }}
         onClick={() => setExpanded(prev => !prev)}
       >
         <h3
           class={`
-            font-body text-#333
+            text-#333 flex flex-wrap items-center gap-2
             ${
               experience.isSubproject
                 ? 'text-lg md:text-xl'
@@ -86,37 +86,35 @@ const Item: Component<Props> = ({ experience, subprojectMap }) => {
             )}
           </For>
           <Show when={experience.pageUrl}>
-            <div class="inline-block">
-              <a
-                href={experience.pageUrl}
-                target="_blank"
-                rel="noopener noreferrer"
-                class="mx-1 flex items-center"
-                onClick={e => e.stopPropagation()}
+            <a
+              href={experience.pageUrl}
+              target="_blank"
+              rel="noopener noreferrer"
+              class="mx-1 h-fit leading-1"
+              onClick={e => e.stopPropagation()}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="currentColor"
+                class="bi bi-house-door-fill w-5 h-5 md:w-6 md:h-6 inline-block"
+                viewBox="0 0 16 16"
               >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="currentColor"
-                  class="bi bi-house-door-fill w-5 h-5 md:w-6 md:h-6 inline-block"
-                  viewBox="0 0 16 16"
-                >
-                  <path d="M6.5 14.5v-3.505c0-.245.25-.495.5-.495h2c.25 0 .5.25.5.5v3.5a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5z" />
-                </svg>
-              </a>
-            </div>
+                <path d="M6.5 14.5v-3.505c0-.245.25-.495.5-.495h2c.25 0 .5.25.5.5v3.5a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5z" />
+              </svg>
+            </a>
           </Show>
           <Show when={experience.githubUrl}>
             <a
               href={experience.githubUrl}
               target="_blank"
               rel="noopener noreferrer"
-              class="mx-1"
+              class="mx-1 h-fit flex"
               onClick={e => e.stopPropagation()}
             >
               <SmoothIcon
                 iconUrl="https://cdn.svgporn.com/logos/github-icon.svg"
                 iconAlt="Github Icon"
-                class="w-5 h-5 md:w-6 md:h-6 mx-2 inline-block"
+                class="w-5 md:w-6 inline-block"
               />
             </a>
           </Show>
