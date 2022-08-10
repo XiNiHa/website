7a399087fded6909f776d0d96373c95ca6a96130
XiNiHa <me@xiniha.dev>
2022-08-10 12:22:22 +0900

    UnoCSS 버전업 및 astro-uno 사용

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,31 +1,33 @@
 import { defineConfig } from 'astro/config'
 import solid from '@astrojs/solid-js'
 import yaml from '@rollup/plugin-yaml'
-import unocss from 'unocss/vite'
+import unocss from 'astro-uno'
 import { presetUno, presetIcons, transformerVariantGroup } from 'unocss'
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [
-      yaml(),
-      unocss({
-        presets: [presetUno(), presetIcons()],
-        transformers: [transformerVariantGroup()],
-        extractors: [
-          {
-            extract: ({ code }) =>
-              new Set(
-                code
-                  .split(/[(\s'"`;=)]|(?:\\")+/g)
-                  .filter(v =>
-                    /(?!\d|-{2}|-\d)[a-zA-Z0-9\u00A0-\uFFFF-_:%-?]/.test(v)
-                  )
-              ),
-          },
-        ],
-      }),
-    ],
+    plugins: [yaml()],
   },
-  integrations: [solid()],
+  integrations: [
+    solid(),
+    unocss({
+      presets: [presetUno(), presetIcons()],
+      transformers: [transformerVariantGroup()],
+      /*
+      extractors: [
+        {
+          extract: ({ code }) =>
+            new Set(
+              code
+                .split(/[(\s'"`;=)]|(?:\\")+/g)
+                .filter(v =>
+                  /(?!\d|-{2}|-\d)[a-zA-Z0-9\u00A0-\uFFFF-_:%-?]/.test(v)
+                )
+            ),
+        },
+      ],
+      */
+    }),
+  ],
 })
