721474bc33419e183b897d7869a69a0c461cba49
Iha Shin <me@xiniha.dev>
2026-09-01 19:36:44 +0900

    섹션 구성 조정

--- src/content.config.ts
+++ src/content.config.ts
@@ -3,14 +3,6 @@ import { z } from 'astro/zod'
 import { defineCollection } from 'astro:content'
 
 export const collections = {
-  techs: defineCollection({
-    loader: glob({ pattern: '*/techs/*.md', base: './src/contents' }),
-    schema: z.object({
-      title: z.string(),
-      order: z.number().positive(),
-      iconUrl: z.string().url(),
-    }),
-  }),
   experiences: defineCollection({
     loader: glob({ pattern: '*/experiences/**/*.md', base: './src/contents' }),
     schema: z.object({
